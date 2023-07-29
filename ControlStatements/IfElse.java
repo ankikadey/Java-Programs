ba0420249c8b4ab2291195164f58fad938851439
Ankika Dey <98796194+ankikadey@users.noreply.github.com>
2023-07-29 22:35:35 +0530

    Update IfElse.java

--- ControlStatements/IfElse.java
+++ ControlStatements/IfElse.java
@@ -3,7 +3,7 @@ public class IfElse {
     public static void main(String[] args) {
         //check even odd
         int a = 24;
-        if(a%2==0){
+        if(a % 2 == 0){ //if the remainder is 0, then the number is considered as even number
             System.out.println("The number is even");
         }
         else{
