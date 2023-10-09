35ff4f17f3ba422f7b930a20ee1bc9c8f5c5da99
Ankika Dey <ankikadey@outlook.com>
2023-10-10 00:22:13 +0530

    program to find missing values

---
+++ Arrays/FindMissingValue.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class FindMissingValue {
+
+    public static int findMissingValue(int[] a, int N){
+        for (int i = 1; i < N; i++) { //outer loop for each element
+            int flag = 0; //to indicate if the value is present
+
+            for(int j = 0; j < N-1; j++) { //inner loop to find the element in the array
+                if (a[j] == i){
+                    flag = 1; //i or the value is present
+                    break;
+                }
+            }
+
+            if(flag == 0){ //if the value is not present
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int N = 5;
+        int[] arr = {1, 2, 3, 5}; //3 is missing
+        System.out.println("Missing value is: " + findMissingValue(arr, N));
+    }
+}
