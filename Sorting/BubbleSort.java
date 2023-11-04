0e054fc94a69dc2c9469a267df831b9416fb7841
Ankika Dey <ankikadey@outlook.com>
2023-11-05 00:03:35 +0530

    add sorting program

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,27 @@
+package Sorting;
+
+class BubbleSort {
+    public static void bubbleSort(int[] arr){
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n-1; i++) { // outer loop for passes
+            for (int j = 1; j < n-i ; j++) { //inner loop for comparisons
+                if (arr[j-1] > arr[j]) {
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                } 
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " "); 
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1}; //worst case
+        bubbleSort(arr);
+        
+    }
+}
