3e11621e53cbcf3185f188a7743596d3dc16522c
Ankika Dey <ankikadey@outlook.com>
2024-01-05 00:53:56 +0530

    add ArrayList.java

---
+++ Arrays/ArrayListExample.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ArrayListExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList <Integer> list = new ArrayList<>(5);
+
+        for (int i = 0; i < 5; i++) {
+            list.add(sc.nextInt());
+        }
+
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+
+        list.set(2, 77);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print(list.get(i) + " ");
+        }
+    }
+}
