e4db54d09d7c32e4ed8af1a9afc9f29c4a1c1c17
Ankika Dey <ankikadey@outlook.com>
2024-01-08 01:00:48 +0530

    add code

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,25 @@
+package Sorting;
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5,1,4,2,3};
+
+        for(int i = 0; i < arr.length-1; i++){
+            int smallest = i;
+            for (int j = i+1; j < arr.length; j++) {
+                //find the smallest value
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
+                }
+            }
+
+            //swap the values
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
