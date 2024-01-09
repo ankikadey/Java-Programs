5488a680ea8a260de4d037ba0545726b58fb61ef
Ankika Dey <ankikadey@outlook.com>
2024-01-09 23:25:52 +0530

    update BubleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,33 +1,25 @@
 import java.util.Arrays;
-public class BubbleSort{
-
-    public static void main(String[] args) {
-        int[] arr = {5, 3, 4, 1, 2};
-        bubble(arr);
-        System.out.println(Arrays.toString(arr));
-    }
-
-
-    public static void bubble(int[] arr) {
-        boolean swapped;
-        // run the steps n-1 times
-        for (int i = 0; i < arr.length; i++) {
+class BubbleSort{
+    public static void bubble(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            boolean swapped;
             swapped = false;
-            // for each step, max item will come at the last respective index
-            for (int j = 1; j < arr.length - i; j++) {
-                // swap if the item is smaller than the previous item
+            for (int j = 1; j <= arr.length-i-1; j++) {
                 if (arr[j] < arr[j-1]) {
-                    // swap
-                    int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
                     swapped = true;
                 }
             }
-            // if you did not swap for a particular value of i, it means the array is sorted hence stop the program
-            if (!swapped) { // !false = true
+            if (!swapped) { //to check if the array is finally sorted or not
                 break;
             }
         }
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void main(String[] args) {
+        int[] arr={5,4,3,2,1};
+        bubble(arr);
     }
 }
