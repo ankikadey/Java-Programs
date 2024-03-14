e83c1aca9cbcd75a687c15d3741cf328764eec10
Ankika Dey <ankikadey@outlook.com>
2024-03-15 00:09:48 +0530

    create a class named Student

---
+++ OOPs/Student.java
@@ -0,0 +1,22 @@
+class Student{
+    String name;
+    int roll;
+    int standard;
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+    }
+}
+
+// class Car{
+//     int wheels;
+//     int seats;
+//     int engine;
+//     String brand;
+// }
+
+// class Main{
+//     public static void main(String[] args) {
+//         Student s1 = new Student(); //object creation
+//     }
+// }
